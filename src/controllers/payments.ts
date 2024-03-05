05404f4a1601b3f09dd3d4bb5e51cdca22208316
kavya sharma <yobrochill56@gmail.com>
2024-03-05 22:55:00 +0530

    changes

--- src/controllers/payments.ts
+++ src/controllers/payments.ts
@@ -211,8 +211,8 @@ export const CreateSession: RequestHandler<
             quantity: 1,
           },
         ],
-        success_url: "https://pixelstorezy.netlify.app/payment/success/" + postid,
-        cancel_url: "https://pixelstorezy.netlify.app/detail/" + postid,
+        success_url: "http://localhost:3000/payment/success/" + postid,
+        cancel_url: "http://localhost:3000/detail/" + postid,
       });
     }else{
       session = await stripe.checkout.sessions.create({
