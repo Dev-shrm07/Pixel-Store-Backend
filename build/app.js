15c82b24a4c42ff1c0c1890144c647d3a6b48c27
kavya sharma <yobrochill56@gmail.com>
2024-09-17 01:31:07 +0530

    jdbsj

--- build/app.js
+++ build/app.js
@@ -3,14 +3,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const https_1 = __importDefault(require("https"));
 const validateENV_1 = __importDefault(require("./utils/validateENV"));
 const app_1_1 = __importDefault(require("./app_1"));
 const mongoose_1 = __importDefault(require("mongoose"));
 require("dotenv/config");
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
+// Load SSL certificates
+const options = {
+    key: fs_1.default.readFileSync('./certs/server.key'),
+    cert: fs_1.default.readFileSync('./certs/server.crt'),
+};
+// Connect to MongoDB
 mongoose_1.default.connect(validateENV_1.default.MONGO_CONNECTION_STRING).then(() => {
-    console.log("db connected");
-    app_1_1.default.listen(port, () => {
-        console.log("app is running fine");
+    console.log("DB connected");
+    // Create HTTPS server
+    https_1.default.createServer(options, app_1_1.default).listen(port, () => {
+        console.log(`App is running on https://localhost:${port}`);
     });
+}).catch((error) => {
+    console.error("Error connecting to DB:", error);
+    process.exit(1);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
-import env from "./utils/validateENV"
-import app from "./app_1"
-import mongoose from "mongoose"
-import express from "express"
-import "dotenv/config"
-import Razorpay from "razorpay"
+import fs from 'fs';
+import https from 'https';
+import env from "./utils/validateENV";
+import app from "./app_1";
+import mongoose from "mongoose";
+import "dotenv/config";
 
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000;
 
+// Load SSL certificates
+const options = {
+  key: fs.readFileSync('./certs/server.key'),
+  cert: fs.readFileSync('./certs/server.crt'),
+};
 
-mongoose.connect(env.MONGO_CONNECTION_STRING).then(()=>{
-    console.log("db connected")
-    app.listen(port, ()=>{
-        console.log("app is running fine")
-    })
-})
+// Connect to MongoDB
+mongoose.connect(env.MONGO_CONNECTION_STRING).then(() => {
+    console.log("DB connected");
+    
+    // Create HTTPS server
+    https.createServer(options, app).listen(port, () => {
+        console.log(`App is running on https://localhost:${port}`);
+    });
+}).catch((error) => {
+    console.error("Error connecting to DB:", error);
+    process.exit(1);
+});
